fb40269acf2d4953d2c1ae0985b6892a926018af
Anurag Sanghvi <creative_maverick@outlook.com>
2020-12-11 01:56:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😊": "Smiling Face with Smiling Eyes",
+  "😇": "Smiling Face with Halo",
+  "🥰": "Smiling Face with Hearts",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😗": "Kissing Face",
+  "😚": "Kissing Face with Closed Eyes",
+  "😙": "Kissing Face with Smiling Eyes",
+  "😋": "Face Savoring Food",
+  "😛": "Face with Tongue",
+  "😜": "Winking Face with Tongue",
+  "😝": "Squinting Face with Tongue",
+  "🤑": "Money-Mouth Face",
+  "🤗": "Hugging Face",
+  "🤭": "Face with Hand Over Mouth",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😏": "Smirking Face",
+  "😔": "Pensive Face",
+  "😪": "Sleepy Face",
+  "🤤": "Drooling Face",
+  "😴": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤒": "Face with Thermometer",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🥴": "Woozy Face",
+  "😵": "Dizzy Face",
+  "🤯": "Exploding Head",
+  "🤠": "Cowboy Hat Face",
+  "🥳": "Partying Face",
+  "😎": "Smiling Face with Sunglasses",
+  "🤓": "Nerd Face",
+  "🧐": "Face with Monocle",
+  "😕": "Confused Face",
+  "😟": "Worried Face",
+  "🙁": "Slightly Frowning Face",
+  "😮": "Face with Open Mouth",
+  "😯": "Hushed Face",
+  "😲": "Astonished Face",
+  "😳": "Flushed Face",
+  "🥺": "Pleading Face",
+  "😦": "Frowning Face with Open Mouth",
+  "😧": "Anguished Face",
+  "😨": "Fearful Face",
+  "😰": "Anxious Face with Sweat",
+  "😥": "Sad but Relieved Face",
+  "😢": "Crying Face",
+  "😭": "Loudly Crying Face",
+  "😱": "Face Screaming in Fear",
+  "😖": "Confounded Face",
+  "😣": "Persevering Face",
+  "😞": "Disappointed Face",
+  "😓": "Downcast Face with Sweat",
+  "😩": "Weary Face",
+  "😫": "Tired Face",
+  "🥱": "Yawning Face",
+  "😤": "Face with Steam From Nose"
+};
+
+var emojies = Object.keys(emojiDictionary);
+
+export default function App() {
+  let [emoji, setEmoji] = useState("");
+  let [meaning, setMeaning] = useState("Translation will come here!");
+
+  function emojiHandler() {
+    let selectedEmoji = event.target.value;
+    setEmoji(selectedEmoji);
+    let newMeaning = emojiDictionary[selectedEmoji];
+    if (newMeaning === undefined) {
+      meaning = "Sorry we dont have that with us right now!";
+    }
+    setMeaning(newMeaning);
+  }
+
+  function emojiClickHandler(selectedEmoji) {
+    setEmoji(selectedEmoji);
+    setMeaning(emojiDictionary[selectedEmoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>
+        Welcome <i className="fa fa-heart"></i>
+      </h1>
+      <h2>Enter a emoji or select one from below to find out what it means.</h2>
+      <input value={emoji} onChange={emojiHandler} />
+      <p className="emojiMeaning">{meaning}</p>
+      <div className="emoji-list">
+        {emojies.map(function (item, index) {
+          return (
+            <span key={index} onClick={() => emojiClickHandler(item)}>
+              {item}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
